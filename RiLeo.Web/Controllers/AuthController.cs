497828d4157c092f9df88d0778e8d4f87b2b5104
rishavraj <fullstackdev.rishav@gmail.com>
2025-11-26 21:08:59 +0530

    Registration and Login - part 1

---
+++ RiLeo.Web/Controllers/AuthController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RiLeo.Web.Models;
+using RiLeo.Web.Service.IService;
+using RiLeo.Web.Utility;
+
+namespace RiLeo.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new ();
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text=SD.RoleAdmin, Value=SD.RoleAdmin},
+                new SelectListItem{Text=SD.RoleCustomer, Value=SD.RoleCustomer}
+
+            };
+
+            ViewBag.RoleList = roleList;
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDto obj)
+        {
+            ResponseDto result = await _authService.RegisterAsync(obj);
+            ResponseDto assignRole;
+
+            if(result != null && result.IsSucess)
+            {
+                if (string.IsNullOrEmpty(obj.Role))
+                {
+                    obj.Role = SD.RoleCustomer;
+                }
+                assignRole = await _authService.AssignRoleAsync(obj);
+                if (assignRole != null && assignRole.IsSucess)
+                {
+                    TempData["success"] = "Registration Successful";
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text=SD.RoleAdmin, Value=SD.RoleAdmin},
+                new SelectListItem{Text=SD.RoleCustomer, Value=SD.RoleCustomer}
+
+            };
+
+            ViewBag.RoleList = roleList;
+
+            return View(obj);
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
