513ea5f3189008a9a0222bc3fe6a0d7eebe3de2a
rishavraj <fullstackdev.rishav@gmail.com>
2025-12-08 20:18:04 +0530

    Add token in the header for Coupon Api

--- RiLeo.Web/Service/BaseService.cs
+++ RiLeo.Web/Service/BaseService.cs
@@ -11,11 +11,13 @@ namespace RiLeo.Web.Service
     public class BaseService : IBaseService
     {
         private readonly IHttpClientFactory _httpClientFactory;
-        public BaseService(IHttpClientFactory httpClientFactory)
+        private readonly ITokenProvider _tokenProvider;
+        public BaseService(IHttpClientFactory httpClientFactory, ITokenProvider tokenProvider)
         {
             _httpClientFactory = httpClientFactory;
+            _tokenProvider = tokenProvider;
         }
-        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto, bool withBearer = true)
         {
             try 
             {
@@ -24,6 +26,11 @@ namespace RiLeo.Web.Service
                 message.Headers.Add("accept", "application/json");
 
                 //token for later
+                if (withBearer)
+                {
+                    var token = _tokenProvider.GetToken();
+                    message.Headers.Add("Authorization", $"Bearer {token}");
+                }
 
                 message.RequestUri = new Uri(requestDto.Url);
                 if (requestDto.Data != null)
